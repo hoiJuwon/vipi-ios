dae7fef7836e55ffbb183f6eee462cf5afe577df
hoiJuwon <withjuwon@gmail.com>
2026-08-27 12:30:37 +0900

    remove motion diagnostic after device verification

--- Vipi/App/VipiApp.swift
+++ Vipi/App/VipiApp.swift
@@ -131,10 +131,6 @@ struct VipiApp: App {
     private let holdsSplashForPreview: Bool
 
     init() {
-        UserDefaults.standard.set(
-            UIAccessibility.isReduceMotionEnabled,
-            forKey: "vipi.diagnostics.reduceMotionEnabled"
-        )
         #if DEBUG
         let allowsLocalhost = CommandLine.arguments.contains("--uitesting")
         #else
